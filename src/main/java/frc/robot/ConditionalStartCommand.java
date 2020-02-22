90d914f47f7d8a339c27b1f50bc9522a9cfb0047
EmmaB3 <30680691+EmmaB3@users.noreply.github.com>
2020-02-22 00:03:17 -0800

    added conditionalstartcommand

---
+++ src/main/java/frc/robot/ConditionalStartCommand.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * Works like wpilib ConditionalCommand except it just starts the command & then ends itself, instead of continuing to run until
+ * started command is done running.
+ */
+public abstract class ConditionalStartCommand extends CommandBase{
+    Command onTrue, onFalse;
+    BooleanSupplier condition;
+
+    public ConditionalStartCommand(Command onTrue, Command onFalse, BooleanSupplier condition){
+        this.onTrue = onTrue;
+        this.onFalse = onFalse;
+        this.condition = condition;
+    }
+    
+    public void initialize(){
+        if(condition.getAsBoolean())
+            onTrue.schedule();
+        else
+            onFalse.schedule();
+
+    }
+
+    public boolean isFinished(){
+        return true;
+    }
+
+}
